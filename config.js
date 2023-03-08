4223b2af7fdeca510d16e94756721a8cb7cec474
T_Kun <93302341+devTMT@users.noreply.github.com>
2023-03-08 18:25:12 +0700

    commit first

---
+++ config.js
@@ -0,0 +1,10 @@
+const nameGirl = 'Em';
+const giftUrl = 'https://www.facebook.com/profile.php?id=100077037485952';
+const eventName = 'Chúc Mừng 8/3';
+const titleCard = 'tặng Thanh Tuyền';
+const contentCard = 'Ngày 8/3 chúc một nửa thế giới luôn thành công trong cuộc sống! Chúc bạn luôn duyên dáng và xinh đẹp trong mắt một nửa thế giới còn lại nhé cô gái!';
+
+
+const giftImage = 'girl.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
